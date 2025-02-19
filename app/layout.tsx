e2981d324dbc25ddd4c3892995a22c7321a88d4d
Divyanshu Dhruv <divyanshudhruv24@gmail.com>
2025-02-19 13:44:50 +0530

    📊 analytics(next): add NextJS analytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,12 +6,13 @@ import Footer from "@/components/footer";
 import ThemeSwitch from "@/components/theme-switch";
 import ThemeContextProvider from "@/context/theme-context";
 import { Toaster } from "react-hot-toast";
-
+import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Divyanshu Dhruv | Portfolio",
-  description: "Divyanshu is a full-stack developer with 8 years of experience.",
+  description:
+    "Divyanshu is a full-stack developer with 8 years of experience.",
 };
 
 export default function RootLayout({
@@ -26,7 +27,7 @@ export default function RootLayout({
       >
         <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] "></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
-
+        <Analytics />
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
             <Header />
