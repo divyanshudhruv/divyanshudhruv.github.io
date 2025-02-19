67e5df8691be26f4d153043e1899e88f383f87dc
Divyanshu Dhruv <divyanshudhruv8@gmail.com>
2025-02-19 14:27:44 +0530

    😓 header(title): update portfolio title AGAIN

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
 import { SpeedInsights } from "@vercel/speed-insights/next";
 export const metadata = {
-  title: "Divyanshu Dhruv | Personal Portfolio",
+  title: "Divyanshu Dhruv | My Portfolio",
   description:
     "Divyanshu is a full-stack developer with 8 years of experience.",
 };
