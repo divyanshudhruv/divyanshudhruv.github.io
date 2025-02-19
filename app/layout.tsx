08fcd8636b3521005885cb2c7e0d350cdcfd70c2
Divyanshu Dhruv <divyanshudhruv24@gmail.com>
2025-02-19 14:04:41 +0530

    📊 analytics(next): add NextJS speedInsights tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import ThemeContextProvider from "@/context/theme-context";
 import { Toaster } from "react-hot-toast";
 import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 export const metadata = {
   title: "Divyanshu Dhruv | Portfolio",
   description:
@@ -28,6 +28,7 @@ export default function RootLayout({
         <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] "></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
         <Analytics />
+        <SpeedInsights />
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
             <Header />
