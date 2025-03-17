ae3f016e8ae62e05c261b9bcfcc909a546ab840a
Divyanshu Dhruv <divyanshudhruv8@gmail.com>
2025-03-17 16:51:05 +0530

    fix linting errors

--- components/intro.tsx
+++ components/intro.tsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 import Link from "next/link";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiOutlineStar } from "react-icons/hi";
-import { FaGithub, FaGithubAlt, FaGithubSquare } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/active-section-context";
 
