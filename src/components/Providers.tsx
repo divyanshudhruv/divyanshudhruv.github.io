8b7f385fbcff0e7780fb2d969effc70fd29ed1a6
Divyanshu Dhruv <divyanshudhruv24@gmail.com>
2025-09-20 00:25:44 +0530

    🥲 fix(flashbang): set theme to "light" in Providers component

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -8,7 +8,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <LayoutProvider>
       <ThemeProvider
-        theme={style.theme as Theme}
+        theme={"light"}
         brand={style.brand as Schemes}
         accent={style.accent as Schemes}
         neutral={style.neutral as NeutralColor}
